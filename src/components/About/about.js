dae30a7f62d84a78793d276cb76e0abfe85c2e5f
Mike <micah41224@gmail.com>
2021-11-28 23:06:05 -0500

    Uncommented about and projects pages and repaired image links to match the new folder structure

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -1,40 +1,40 @@
 import React from 'react';
-// import aboutImg from "../assets/images/profile-pic-2.png";
+import aboutImg from "../../assets/images/profile-pic-2.png";
 
 // const about = () => {
 //     return (
 
 const about = () => {
     return (
-        <div className="container">
-            <h1 className="text-center" style={{paddingTop: "30%"}}>
-                About
-            </h1>
+        // <div className="container">
+        //     <h1 className="text-center" style={{paddingTop: "30%"}}>
+        //         About
+        //     </h1>
 
-        </div>
-    
-        // <div className="container py-5 about-container aboutContainer">
-        //    <div className="row text-center">
-        //        <div className="col-lg-6 col-xs-12">
-        //             <div className="photo-wrap mb-5">
-        //                 <img className="about-img" src={aboutImg} alt="aboutImg"></img>
-        //             </div>
-        //        </div>
-        //        <div className="col-lg-6 col-xs-12">
-        //             <h1 className="about">
-        //                About
-        //             </h1>
-        //             <h6 className="aboutText">
-        //                 Hello, my name is Michael Feldman and welcome to my portfolio site! It is a genuine pleasure to be able to share my
-        //                 work with you and thank you for taking the time to have a look around! Below linked is a sample of my work and the
-        //                 skills I have attained thus far in my coding journey. Snellville Georgia has been home to me for the last 30 years
-        //                 and is short jaunt from the city of Atlanta. My passions include reading, exercise, movies and gaming! Though early
-        //                 in my coding journey its something I'm already deeply enjoying learning as it can be seen as one massive puzzle with
-        //                 so many potential paths to completion.
-        //             </h6>
-        //        </div>
-        //     </div>
         // </div>
+    
+        <div className="container py-5 about-container aboutContainer">
+           <div className="row text-center">
+               <div className="col-lg-6 col-xs-12">
+                    <div className="photo-wrap mb-5">
+                        <img className="about-img" src={aboutImg} alt="aboutImg"></img>
+                    </div>
+               </div>
+               <div className="col-lg-6 col-xs-12">
+                    <h1 className="about">
+                       About
+                    </h1>
+                    <h6 className="aboutText">
+                        Hello, my name is Michael Feldman and welcome to my portfolio site! It is a genuine pleasure to be able to share my
+                        work with you and thank you for taking the time to have a look around! Below linked is a sample of my work and the
+                        skills I have attained thus far in my coding journey. Snellville Georgia has been home to me for the last 30 years
+                        and is short jaunt from the city of Atlanta. My passions include reading, exercise, movies and gaming! Though early
+                        in my coding journey its something I'm already deeply enjoying learning as it can be seen as one massive puzzle with
+                        so many potential paths to completion.
+                    </h6>
+               </div>
+            </div>
+        </div>
     )
 }
 
